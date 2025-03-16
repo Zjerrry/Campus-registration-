fc46c1dbbbb91c17cef17cccdd83455677102ee6
TensonC <2285140013ctx@gmail.com>
2025-03-16 12:39:02 +0800

    3.16优化界面，解决了文字溢出问题，优化搜所框搜索

--- lib/routes/ClubListPage.dart
+++ lib/routes/ClubListPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ClubMainPage.dart';
 
-List allClub = ["天外天工作室","青年志愿者协会","学生会","天津大学校园建设发展协会（北洋讲解队）","abcdef","abc"];//全部社团列表
+List allClub = ["天外天工作室","青年志愿者协会XXXXXXXXXXXXXXXXXXX","学生会","天津大学校园建设发展协会（北洋讲解队）","abcdef","abc"];//全部社团列表
 List searchClub = [];//筛选后的社团列表
 
 class Clublistpage extends StatefulWidget {
@@ -72,39 +72,40 @@ class _TopSearchBarState extends State<TopSearchBar> {
       child:Padding(
           padding: EdgeInsets.all(5),
           child:TextField(
-        controller: _controller,
-        focusNode: _focusNode,
-        autofocus: false,
-        decoration: InputDecoration(
-            hintText: widget.lable,
-            hintStyle: TextStyle(color: Colors.grey),
-            border: InputBorder.none,
-            icon: const Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: Icon(Icons.search,
-                size: 18,
-                color: Colors.blue,
-              ),),
-            //输入后显示的清除输入内容按键
-            suffixIcon: _ClublistpageState.SearchValue.isNotEmpty ?
-            IconButton(onPressed: ()
-            {
+            controller: _controller,
+            focusNode: _focusNode,
+            autofocus: false,
+            decoration: InputDecoration(
+                hintText: widget.lable,
+                hintStyle: TextStyle(color: Colors.grey),
+                border: InputBorder.none,
+                icon: const Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Icon(Icons.search,
+                    size: 20,
+                    color: Colors.blue,
+                  ),),
+                //输入后显示的清除输入内容按键
+                suffixIcon: _ClublistpageState.SearchValue.isNotEmpty ?
+                  IconButton(onPressed: ()
+                  {
+                    setState(() {
+                      _ClublistpageState.SearchValue = "";
+                      _controller.clear();
+                      //Search(_ClublistpageState.SearchValue);
+                    });
+                  }, icon: Icon(Icons.close)):null,
+
+                ),
+            onChanged: (value){
               setState(() {
-                _ClublistpageState.SearchValue = "";
-                _controller.clear();
-                //Search(_ClublistpageState.SearchValue);
+                _ClublistpageState.SearchValue = value;
               });
-            }, icon: Icon(Icons.close)):null
-            ),
-        onChanged: (value){
-          setState(() {
-            _ClublistpageState.SearchValue = value;
-          });
-        },
-        onSubmitted: (value){
-          widget.onSearch(value);
-        },
-      ),
+            },
+            onSubmitted: (value){
+              widget.onSearch(value);
+              },
+        ),
       ),
 
     );
@@ -126,53 +127,81 @@ class _ClubList_BodyState extends State<ClubList_Body> {
     return GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
-            childAspectRatio: 0.7
+            childAspectRatio: 0.8
         ),
         itemCount: _ClublistpageState.SearchValue.isNotEmpty ? searchClub.length:allClub.length,
         itemBuilder: (context,index){
           return Padding(
               padding: const EdgeInsets.all(8.0),
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context)
-                          {
-                            return ClubMainPage(
-                              clubname:_ClublistpageState.SearchValue.isNotEmpty ? searchClub[index]:allClub[index] ,
-                            );
-                          })
-                  );
-                },//点击后打开社团介绍页
-                child: Container(
-                    alignment: Alignment.bottomCenter,
+              child:  Container(
+                    //alignment: Alignment.center,
                     decoration: BoxDecoration(
-                      //color: Colors.blue,
-                        borderRadius: BorderRadius.circular(20)
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      boxShadow: [BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        spreadRadius: 2,
+                        blurRadius: 5
+                      ),]
                     ),
-                    child: Column(
-                      children: [
-                        Image.network("https://tse2-mm.cn.bing.net/th/id/OIP-C.OTqjOW2I-11mg43Ki0q5wwHaHa?w=186&h=186&c=7&r=0&o=5&dpr=2.1&pid=1.7"),
-                        Expanded(
-                            child: Padding(
-                              padding: EdgeInsets.all(20.0),
-                              child: Text("${_ClublistpageState.SearchValue.isNotEmpty ? searchClub[index]:allClub[index]}",
-                                style: TextStyle(fontSize: 20),
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(20),
+                      onTap: (){
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context)
+                                {
+                                  return ClubMainPage(
+                                    clubname:_ClublistpageState.SearchValue.isNotEmpty ? searchClub[index]:allClub[index] ,
+                                  );
+                                })
+                        );
+                      },
+                      child: AspectRatio(
+                          aspectRatio: 1.065,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              ClipRRect(
+                                borderRadius:  const BorderRadius.vertical(top: Radius.circular(20)),
+                                child: AspectRatio(
+                                    aspectRatio: 1,//图片尺寸
+                                  child: Image.network("https://s3-alpha-sig.figma.com/img/4a92/272c/15495ba4b99f0c27386610b0fb8af758?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=j3j6eWQEj56i-AS6o9HDN0GG9mMKTsfOe4pZH08fLvHvAi~Wcpm8VRO-93vlIChrYffVEpQbKG9xm2wWI7EwtcWfSoPxdjrVmCKD5thR2oDVwk3YfSvqWLltzuYO6CXgcPqHxhmvtODVNnQstDLGT7iXtlJJ7-8F5mFUnXFTDQI035DX2BGvuCoL2ad8rhQhy8Y6lA36z1a3TTbwF1cXE216kD6d4WTXWx6jakwSaeW9~1gNZipuD~PIeRkRxmYImDaY2Z8lYCFJjlbC138s6gthr7DhhjznTTT99-n-4XHRJP6XAWMRjHFtF-oKMGUWCGyyPNEMKAafeEm9AUY~uQ__",
+                                      fit: BoxFit.cover,
+                                      errorBuilder: (_, __, ___) => Container( // 错误处理
+                                        color: Colors.grey[200],
+                                        child: const Icon(Icons.broken_image),
+                                    ),
+                                  ),
+                                ),
                               ),
-                            )
-                        )
-                      ],
+                              Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 8),
+                                child: Text("${_ClublistpageState.SearchValue.isNotEmpty ? searchClub[index]:allClub[index]}",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    height: 1.2
+                                  ),
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  textAlign: TextAlign.center,
+                                ),
+                              )
+                            ],
+                          ),
+                      ),
                     )
                 ),
-              )
+
           );
         }
     );
   }
 }
-
-void Search(String SearchValue)//搜索筛选的实现
+//搜索筛选的实现
+void Search(String SearchValue)
 {
   searchClub = [];//初始化筛选列表
   if(SearchValue == "") {
@@ -182,10 +211,68 @@ void Search(String SearchValue)//搜索筛选的实现
     {
       if(allClub[i].length<SearchValue.length)
         continue;
-      if(allClub[i].substring(0,SearchValue.length) == SearchValue)
+      if(StrStr(allClub[i], SearchValue) >= 0)
         {
           searchClub.add(allClub[i]);
         }
     }
+}
+//KMP算法实现搜索内容对比
+int StrStr(String Clubname,String Searchvalue)
+{
+  List next = build_next(Searchvalue);
+
+  int i = 0;
+  int j = 0;
+  while(i < Clubname.length)
+    {
+      if(Clubname[i] == Searchvalue[j])
+      {
+        i++;
+        j++;
+      }
+      else if(j>0)
+        {
+          j = next[j-1];
+        }
+      else
+        {
+          i++;
+        }
+      if(j == Searchvalue.length)
+      {
+        return i-j;
+      }
+    }
+    return -1;
+}
+
+List build_next(String Searchvalue)
+{
+  List next = [0];
+  int prefix_len = 0;
+  int i = 1;
+  while(i<Searchvalue.length)
+    {
+      if(Searchvalue[prefix_len] == Searchvalue[i])
+        {
+          prefix_len++;
+          next.add(prefix_len);
+          i++;
+        }
+      else
+        {
+          if(prefix_len == 0)
+            {
+              next.add(0);
+              i++;
+            }
+          else
+          {
+            prefix_len = next[prefix_len];
+          }
+        }
+    }
+    return next;
 }
 
--- lib/routes/ClubMainPage.dart
+++ lib/routes/ClubMainPage.dart
@@ -42,45 +42,64 @@ Widget Club_Information(context,ClubName)
     child: Container(
       child: Column(
         children: [
+          //社团信息部分
           Padding(
             padding: const EdgeInsets.all(12.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Icon(Icons.person,size: 100,),//社团头像
-                Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(ClubName,style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 25
-                      ),),//社团名称
-                      Text("类型:  育人实践基地")//社团类型
-                    ],//！！！！！！！！！！！会溢出屏幕！！！！！！！！！
-                  ),
+                Image.network("https://s3-alpha-sig.figma.com/img/4a92/272c/15495ba4b99f0c27386610b0fb8af758?Expires=1742774400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=j3j6eWQEj56i-AS6o9HDN0GG9mMKTsfOe4pZH08fLvHvAi~Wcpm8VRO-93vlIChrYffVEpQbKG9xm2wWI7EwtcWfSoPxdjrVmCKD5thR2oDVwk3YfSvqWLltzuYO6CXgcPqHxhmvtODVNnQstDLGT7iXtlJJ7-8F5mFUnXFTDQI035DX2BGvuCoL2ad8rhQhy8Y6lA36z1a3TTbwF1cXE216kD6d4WTXWx6jakwSaeW9~1gNZipuD~PIeRkRxmYImDaY2Z8lYCFJjlbC138s6gthr7DhhjznTTT99-n-4XHRJP6XAWMRjHFtF-oKMGUWCGyyPNEMKAafeEm9AUY~uQ__"), //社团头像
+                Flexible(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(ClubName,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 25
+                        ),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),//社团名称
+                        Text("类型:  育人实践基地",style: TextStyle(
+                          color: Colors.grey[600],fontSize: 17
+                        ),)//社团类型
+                      ],//会溢出屏幕
+                    ),
                 ),
-                Icon(Icons.favorite_border,size: 30,) //是否关注该社团
+                IconButton(icon:Icon(Icons.favorite_border,size: 30,),
+                onPressed: (){}
+                ) //是否关注该社团
               ],),
           ),
-
+          //社团简介部分
           ConstrainedBox(
-              constraints: BoxConstraints(
-                minWidth: double.infinity,
+              constraints:const  BoxConstraints(
                 minHeight: 100
               ),
-            child: const Padding(
-              padding: const EdgeInsets.only(left: 40.0,right: 40),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
               child:  Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
-                    padding: EdgeInsets.only(right: 20),
+                    padding: EdgeInsets.only(right: 20,top: 3),
                     child: Text("社团简介:",style: TextStyle(
-                        fontWeight: FontWeight.w700)),
+                        fontWeight: FontWeight.w700,
+                      color: Colors.grey[600],
+                      fontSize: 16
+                      )
+                    ),
                   ),
                   Expanded(
                     child: Text(
-                        "该社团是XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+                        "该社团是XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        height: 1.5,
+                        fontSize: 14
+                      ),
                     ),
                   )
 
